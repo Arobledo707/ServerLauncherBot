c3b29e665df635eda631eb9a2728720fe7ab14d0
fuel <arobledo707@gmail.com>
2020-05-23 19:04:42 -0700

    Added a space in message sent when adding roles or prefixes

--- DiscordServerBot.py
+++ DiscordServerBot.py
@@ -56,14 +56,14 @@ def parse_json_config():
                 global command_roles
                 if role not in command_roles:
                     command_roles.append(role)
-                    print_and_log(message_type.INFO, 'Adding' + role + 'to command roles')
+                    print_and_log(message_type.INFO, 'Adding ' + role + 'to command roles')
                 else:
                     print_and_log(message_type.INFO, role + ' was already in command roles!')
             for prefix in data['prefixes']:
                 global command_prefix
                 if prefix not in command_prefix:
                     command_prefix.append(prefix)
-                    print_and_log(message_type.INFO, 'Adding' + prefix + 'to bot prefixes')
+                    print_and_log(message_type.INFO, 'Adding ' + prefix + 'to bot prefixes')
                 else:
                     print_and_log(message_type.INFO, prefix + ' is already a prefix!')
     except Exception as e:
