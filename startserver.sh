fa27c452c984caaa038b7472c9d0800211ac991d
Alberto <arobledo707@gmail.com>
2021-08-23 16:45:35 -0700

    updated startserver.sh with comments

--- startserver.sh
+++ startserver.sh
@@ -3,7 +3,9 @@ if pgrep -x "$SERVCE" >/dev/null
 then
 	echo "$SERVICE is running"
 else
+# enter path to terraria directory replace /home/pi/terraria that is just the directory where I had my TShock terraria files
 	cd /home/pi/terraria
-	screen -dmS terraria mono TerrariaServer.exe -players 3 -world /mnt/slowhdd/terraria/The_Lonely_Alan.wld
+	# replace /mnt/slowhdd/terraria/testmap.wld you can customize and of the TerrariaServer.exe flags to how you want to have your server set up.
+	screen -dmS terraria mono TerrariaServer.exe -players 3 -world /mnt/slowhdd/terraria/testmap.wld
 
 fi
